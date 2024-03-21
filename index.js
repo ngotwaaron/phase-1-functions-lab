60b249ef8ba89c1606a118fd8c1d0b22852963fd
ngotwaaron <ngotwaaron@gmail.com>
2024-03-21 22:29:06 +0300

    complete phase-1-functions lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if(block >= 42){
+        return block - 42;
+    }else if(block <= 42)
+        return 42 - block;
+}
+function distanceFromHqInFeet (feet) {
+    if(feet >= 42){
+        return (feet-42) * 264;
+    }else if(feet <= 42)
+        return (42 - feet) * 264;
+}
+function distanceTravelledInFeet(start1, end) {
+    if(start1 >= end){
+        return(start1 - end) * 264;
+    }else if(end >= start1)
+        return(end - start1) * 264;
+}
+function calculatesFarePrice(start, destination){
+    const feetblock = 264;
+    const distance = Math.abs(destination -start);
+    const fare = distance * feetblock;
+    if(fare <= 400 ){
+        return 0;
+    }else if(fare > 400 && fare <= 2000){
+        return (fare - 400) * 0.02;
+    }else if(fare > 2000 && fare <= 2500){
+        return 25;
+    }else {
+        return 'cannot travel that far';
+    }
+}
+
+ 
+     
+
+
+
+
+
+
+
+
+
